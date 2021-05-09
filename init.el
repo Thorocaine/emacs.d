f3b683797d501843a49f07ccd888e243f8a645ce
Jonathan Peel <me@jonathanpeel.co.za>
2021-05-09 08:10:44 +0200

    Some nice file shortcuts

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; The default is 800 kilobytes.  Measured in bytes.
-(setq gc-cons-threshold (* 50 1000 1000))
+(setq gc-cons-threshold (* 100 1000 1000))
 
 (defun efs/display-startup-time ()
   (message "⏱ Emacs loaded in %s with %d garbage collections."
@@ -26,6 +26,7 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+;; (setq use-package-always-defer t)
 
 (use-package auto-package-update
   :custom
@@ -77,55 +78,77 @@
 (set-face-attribute 'variable-pitch nil :font "Cantarell" :height runemacs/default-font-size :weight 'regular)
 (set-fontset-font t '(#x1f300 . #x1fad0) (font-spec :family "Segoe UI Emoji"))
 
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+;; (defun my/open-index ()
+  ;;   "Index"
+  ;;   (interactive)
+  ;;   (find-file (expand-file-name "~/Dropbox/Apps/MobileOrg/index.org"))
+  ;;   )
 
-  (use-package general
-    :after evil
-    :config
-      (general-create-definer rune/leader-keys
-        :keymaps '(normal insert visual emacs)
-        :prefix "SPC"
-        :global-prefix "C-SPC")
-
-      (rune/leader-keys
-        "t"  '(:ignore t :which-key "toggles")
-        "tt" '(counsel-load-theme :which-key "choose theme")
-        "fde" '(lambda () (interactive) (find-file (expand-file-name "~/.emacs.d/Emacs.org")))
-        "e"  '(:ignore t :which-key "eval")
-        "eb" 'eval-buffer
-        "es" 'eval-last-sexp
-        "o"  '(:ignore t :which-key "org")
-        "oi" '(find-file-existing "~/Dropbox/Apps/MobileOrg/index.org")
-        "oc" '(find-file-existing "~/.emacs.d/init.el")
-        "oa" 'org-agenda
-        )
-   )
-
-
-  (use-package evil
-    :init
-    (setq evil-want-integration t)
-    (setq evil-want-keybinding nil)
-    (setq evil-want-C-u-scroll t)
-    (setq evil-want-C-i-jump nil)
-
-    :config
-    (evil-mode 1)
-    (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-    (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-
-    ;; Use visual line motions even outside of visual-line-mode buffers
-    (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-    (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-    (evil-set-initial-state 'messages-buffer-mode 'normal)
-    (evil-set-initial-state 'dashboard-mode 'normal)
+(defun my/open (a)
+  "Open File"
+  (find-file (expand-file-name a))
+  (message "Open")
   )
 
-  (use-package evil-collection
-    :after evil
-    :config
-    (evil-collection-init))
+  (defun my/org-index () (interactive) (my/open "~/Dropbox/Apps/MobileOrg/index.org") )
+  (defun my/sleep () (interactive) (my/open "~/Dropbox/Apps/MobileOrg/sleep.org") )
+  (defun my/work-out () (interactive) (my/open "~/Dropbox/Apps/MobileOrg/workout.org") )
+
+
+      (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+        (use-package general
+          :after evil
+          :config
+            (general-create-definer rune/leader-keys
+              :keymaps '(normal insert visual emacs)
+              :prefix "SPC"
+              :global-prefix "C-SPC")
+
+            (rune/leader-keys
+              "t"  '(:ignore t :which-key "toggles")
+              "tt" '(counsel-load-theme :which-key "choose theme")
+              "fde" '(lambda () (interactive) (find-file (expand-file-name "~/.emacs.d/Emacs.org")))
+              "e"  '(:ignore t :which-key "eval")
+              "eb" 'eval-buffer
+              "es" 'eval-last-sexp
+               "a"  '(:ignore t :which-key "app")
+              "ao"  '(:ignore t :which-key "org")
+              "aof"  '(:ignore t :which-key "files")
+               "aofi" 'my/org-index
+              "aofs" 'my/sleep
+              "aofw" 'my/work-out
+              "aoa" 'org-agenda
+               "g"  '(:ignore t :which-key "git")
+	     "gs" 'magit-status
+              )
+         )
+
+
+        (use-package evil
+          :init
+          (setq evil-want-integration t)
+          (setq evil-want-keybinding nil)
+          (setq evil-want-C-u-scroll t)
+          (setq evil-want-C-i-jump nil)
+
+          :config
+          (evil-mode 1)
+          (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+          (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+          ;; Use visual line motions even outside of visual-line-mode buffers
+          (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+          (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+          (evil-set-initial-state 'messages-buffer-mode 'normal)
+          (evil-set-initial-state 'dashboard-mode 'normal)
+        )
+
+        (use-package evil-collection
+          :after evil
+          :config
+          (evil-collection-init))
 
 (use-package doom-themes
   :init (load-theme 'doom-palenight t))
@@ -599,3 +622,6 @@
     :config
     (evil-collection-define-key 'normal 'dired-mode-map
       "H" 'dired-hide-dotfiles-mode))
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
